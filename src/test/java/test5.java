59a2ebffec9d558b346b09cec4c261727f7686c7
08aBel80 <aberi21@freeuni.edu.ge>
2023-02-08 20:39:56 +0400

    push commit

---
+++ src/test/java/test5.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class test5 {
+    @Test
+    public void test(){
+        int a = 1;
+        int b = 1;
+        Assert.assertEquals(a, b);
+
+    }
+
+}
