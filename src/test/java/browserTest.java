070c17d49bc263902cc424a1e857d7c62a518cda
08aBel80 <aberi21@freeuni.edu.ge>
2023-02-09 16:55:35 +0400

    adding browsertest

---
+++ src/test/java/browserTest.java
@@ -0,0 +1,26 @@
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
+import aquality.selenium.browser.BrowserName;
+import aquality.selenium.core.utilities.ISettingsFile;
+import aquality.selenium.core.utilities.JsonSettingsFile;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class browserTest {
+    Browser browser;
+    ISettingsFile setConfigData = new JsonSettingsFile("settings.json");
+    @BeforeMethod
+    public void setUp(){
+    }
+
+    @Test
+    public void test(){
+        System.out.println(System.getenv("browser"));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+    }
+}
