06359e20e29fb3f96e0d4bb187884d4cd5879fed
08aBel80 <aberi21@freeuni.edu.ge>
2023-02-09 17:46:41 +0400

    push commit

--- src/test/java/browserTest.java
+++ src/test/java/browserTest.java
@@ -1,16 +1,15 @@
 import aquality.selenium.browser.AqualityServices;
-import aquality.selenium.browser.Browser;
-import aquality.selenium.browser.BrowserName;
-import aquality.selenium.core.utilities.ISettingsFile;
-import aquality.selenium.core.utilities.JsonSettingsFile;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.settingsUtils;
 
 public class browserTest {
     @Test
     public void test(){
-        System.out.println("search engine: "+ System.getenv("searchEngine"));
-        System.out.println("chosen browser: " + System.getenv("browser"));
+        String browserName = System.getenv("browser");
+        String url = System.getenv("searchEngine");
+
+        settingsUtils.setBrowserName(browserName);
+
+        AqualityServices.getBrowser().goTo(url);
     }
 }
